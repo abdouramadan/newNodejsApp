68b7ca155dd03d3ffe2be4626a59780983c866b9
abdouramadan <abdouramadan36@gmail.com>
2020-12-04 23:01:13 +0300

    added user ui

--- userAuth/models/User.js
+++ userAuth/models/User.js
@@ -1,9 +1,26 @@
-const mongoose  =   require('mongoose')
+const mongoose    = require('mongoose')
+const { isEmail } = require('validator')
 
 const userSchema = new mongoose.Schema({
-
+    username: {
+        type: String,
+        required: [true, 'please enter your username'],
+        minlength: [5, 'minimum username length is (5) characters'],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, 'please enter your email address'],
+        unique: true,
+        validate: [isEmail, 'please enter valid email address']
+    },
+    username: {
+        type: String,
+        required: [true, 'please enter your password'],
+        minlength: [6, 'minimum password length is (6) characters'],
+    }
 })
 
-const User = mongoose.model('user', userSchema)
+const User = mongoose.model('users', userSchema)
 
 module.exports = User
